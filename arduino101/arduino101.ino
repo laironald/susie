00af17ffdde45d5598e4dd7bfccf2b728837a766
Ron Lai <laironald@gmail.com>
2014-10-09 17:53:51 -0700

    added small sample program

---
+++ arduino101/arduino101.ino
@@ -0,0 +1,42 @@
+#include <avr/pgmspace.h>
+
+String readString;
+
+void setup() 
+{
+  Serial.begin(9600);
+  delay(500);
+}
+
+void loop()
+{
+  /*
+  motion();
+  motionReturn();
+  */
+  
+  /*
+    Convert serial.read() into a useable string using Arduino?
+    http://stackoverflow.com/questions/5697047/convert-serial-read-into-a-useable-string-using-arduino
+    http://stackoverflow.com/questions/24961402/how-to-compare-string-from-serial-read
+  */
+
+  while (Serial.available()) {
+    delay(3);  
+    char c = Serial.read();
+    readString += c; 
+  }
+  if (readString.length() >0) {
+    if (readString == "on") {
+      Serial.println("switching on");
+    } else if (readString == "off") {
+      Serial.println("switching off");
+    } else {
+      Serial.print("This is a string: ");
+      Serial.println(readString);
+    }
+    readString="";
+  }   
+
+} 
+
