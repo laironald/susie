15f241b19b77a1898009c390e20fa549d73bf673
Ron Lai <laironald@gmail.com>
2014-10-11 15:21:48 -0700

    recommit

--- py/uarm.py
+++ py/uarm.py
@@ -4,22 +4,20 @@ import time
 # Arduino Playground
 # http://playground.arduino.cc/Interfacing/Python
 def connect(item = None):
-	if item:
+  if item:
     try:
-  		serial_port = '/dev/cu.usbserial-A6031NA9'
-  		ser = serial.Serial(serial_port, 9600, timeout=1)
-    except:
       serial_port = '/dev/tty.usbmodem1411'
       ser = serial.Serial(serial_port, 9600, timeout=1)
+    except:
+      serial_port = '/dev/cu.usbserial-A6031NA9'
+      ser = serial.Serial(serial_port, 9600, timeout=1)
 
-		time.sleep(2)
+    time.sleep(2)
+    output = []
+    ser.write(item)
+    output.append(ser.readline())
 
-		output = []
-
-		ser.write(item)
-		output.append(ser.readline())
-
-		ser.close()
-		return output
-	else:
-		return [""]
+    ser.close()
+    return output
+  else:
+    return [""]
