9c9df8af57643f0a6ab652cbc414fab2142daa4b
Ron Lai <laironald@gmail.com>
2014-10-19 22:03:36 -0700

    added inos

---
+++ sketches/ron/src/sketch.ino
@@ -0,0 +1,149 @@
+
+String readString;
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while (Serial.available()) {
+    delay(3);  
+    char c = Serial.read();
+    readString += c; 
+  }
+  if (readString.length() > 0) {
+    if (readString == "on") {
+    Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+Serial.println("this is a very long string");
+Serial.println("this is another long string");
+Serial.println("hello");
+
+    }
+    readString="";
+  }
+} 
+    
